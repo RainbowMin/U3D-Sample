6665b02c006964ec329e85cc9f9c671344f5ac65
EaseliveMJ <ecust.mj@gmail.com>
2013-10-14 15:17:42 +0800

    add art resouce;adjust project struct

---
+++ Assets/Script/Script_03_19.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script_03_19 : MonoBehaviour 
+{
+	Object[] animList;
+	int CurFrame;
+	int TotalFrameCount;
+	float FPS = 15;
+	float time = 0;
+
+	//支持中文
+	// Use this for initialization
+	void Start () 
+	{
+		animList = Resources.LoadAll("animation");
+		TotalFrameCount = animList.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	void OnGUI()
+	{
+		DrawAnimation(animList,new Rect(100,100,32,48));
+	}
+	
+	void DrawAnimation(Object[] tex, Rect rect)
+	{
+		GUILayout.Label("当前动画播放：第"+CurFrame+"帧");
+		GUI.DrawTexture(rect, (Texture2D)tex[CurFrame], ScaleMode.StretchToFill, true, 0);
+		time += Time.deltaTime;
+		if(time > 1.0 / FPS)
+		{
+			CurFrame++;
+			time = 0;
+			if(CurFrame >= TotalFrameCount)
+			{
+				CurFrame = 0;
+			}
+		}
+	}
+}
