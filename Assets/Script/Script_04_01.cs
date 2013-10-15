d41666c7ad673edf6e607381b1025e3b7005882e
EaseliveMJ <ecust.mj@gmail.com>
2013-10-15 17:17:34 +0800

    ch3 && ch4_01

---
+++ Assets/Script/Script_04_01.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class Script_04_01 : MonoBehaviour 
+{
+	void Start () 
+	{
+		int i=100;
+		int j=1;
+		Debug.Log(j);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Script/Script_04_02.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Script_04_02 : MonoBehaviour 
+{
+
+	//支持中文
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
